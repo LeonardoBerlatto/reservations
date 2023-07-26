a63b4712ce50cf1c59cca800932e0e8c9da26eab
Leonardo Berlatto <leoberlatto@gmail.com>
2023-07-26 14:12:57 -0300

    feat(initial-dependencies): Add Spring validation and Springdoc

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,15 +22,21 @@ repositories {
 }
 
 dependencies {
+	// spring
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+	// utils
 	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+	// database
 	runtimeOnly("com.h2database:h2")
 	runtimeOnly("io.r2dbc:r2dbc-h2")
 	runtimeOnly("org.postgresql:postgresql")
 	runtimeOnly("org.postgresql:r2dbc-postgresql")
-	annotationProcessor("org.projectlombok:lombok")
+	// tests
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 }
